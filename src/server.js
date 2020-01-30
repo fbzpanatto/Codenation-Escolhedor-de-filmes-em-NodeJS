3d005acad2a1b85f68cf780c212dcd6c9c92a6e4
fbzpanatto <fbzpanatto@hotmail.com>
2020-01-30 12:35:27 -0300

    Alterei a forma de leitura do movies.json

--- src/server.js
+++ src/server.js
@@ -1,17 +1,17 @@
 const express = require('express')
 const app = express()
 const { randomMovie, randomByDirector } = require('./tools/randomMov')
-const { readMovies } = require('./tools/readFile')
+const { parseFile } = require('./tools/readFile')
 
 app.get('/v1/movie', async (req, res, next) => {
-  readMovies()
+    parseFile()
     .then(dados => res.status(200).send(randomMovie(dados)))
     .catch(err => res.status(500).send(err))
 })
 
 
 app.get(`/v1/movie/:director`, async (req, res, next) => {
-  readMovies()
+    parseFile()
     .then(dados => {
       const mov = randomByDirector(dados, req.params.director)
       if (mov) {
@@ -23,7 +23,7 @@ app.get(`/v1/movie/:director`, async (req, res, next) => {
     .catch(err => res.status(500).send(err))
 })
 
-const start = async (port = 8080) => {
+const start = async (port = 1212) => {
   app.listen(port, function () {
     console.info('%s listening at port %s', app.name, port)
   })
